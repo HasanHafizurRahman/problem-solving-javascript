9dc1ccb1d0efd69c71e1e37f84fa00afdf94fa18
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-02-05 07:57:42 +0600

    array indexof

--- lws.js
+++ lws.js
@@ -1,41 +1,42 @@
 // Problem 1] - Generate a random number between 1-6
 
-Math.floor(Math.random() * 7);
+// Math.floor(Math.random() * 7);
 
 // Problem 2] - Sort the names based on Alphabet
-let names = ["John Doe", "Alex Doe", "Peter Doe", "Elon Doe"];
-let sortedNames = names.sort();
-console.log(sortedNames);
+// let names = ["John Doe", "Alex Doe", "Peter Doe", "Elon Doe"];
+// let sortedNames = names.sort();
+// console.log(sortedNames);
 
 // Problem 3] - Sort the number
-const points = [40, 100, 1, 5, 25, 10];
-const sortedPoints = points.sort(function (a, b) {
-  return a - b;
-});
-console.log(sortedPoints);
+// const points = [40, 100, 1, 5, 25, 10];
+// const sortedPoints = points.sort(function (a, b) {
+//   return a - b;
+// });
+// console.log(sortedPoints);
 
 // Problem 4] - Find leap year
-function isLeapyear(year) {
-  if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
-    console.log(`${year} is leap year`);
-  } else {
-    console.log(`${year} isn't a leap year`);
-  }
-}
-isLeapyear(2026);
+// function isLeapyear(year) {
+//   if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+//     console.log(`${year} is leap year`);
+//   } else {
+//     console.log(`${year} isn't a leap year`);
+//   }
+// }
+// isLeapyear(2026);
 
 // Problem 5] - find the vowels inside a sentence
-let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+// let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 
-function findvowels(sentence) {
-  let count = 0;
-  const splitted = Array.from(sentence);
-  const result = splitted.forEach(function (params) {
-    if (vowels.includes(params)) {
-      return count++;
-    }
-  });
-  return count;
-}
+// function findvowels(sentence) {
+//   let count = 0;
+//   const splitted = Array.from(sentence);
+//   const result = splitted.forEach(function (params) {
+//     if (vowels.includes(params)) {
+//       return count++;
+//     }
+//   });
+//   return count;
+// }
 
-console.log(findvowels("I am Hasan Hafizur"));
+// console.log(findvowels("I am Hasan Hafizur"));
+
---
+++ lws1.js
@@ -0,0 +1,1 @@
+// find the duplicate numbers from the array
